a52badc371bfd88e36e27b51fbbf0a4b3aec8d8c
ideal-idiot <77572290+ideal-idiot@users.noreply.github.com>
2021-01-25 10:44:21 +0530

    Update Random_Password_Generator.py

--- Random_Password_Generator.py
+++ Random_Password_Generator.py
@@ -2,11 +2,13 @@ import random as r
 alp=['a','b','c','d','e','f','g','h','i','j','k','l','m','o','p','q','r','s','t','u','v','w','x','y','z']
 num=[1,2,3,4,5,6,7,8,9]
 spe_ch=['!','@','#','$','%','^','&','*','(',')','+']
-while(i<8)
+count=0
+pword=str(0)
+while(count<3):
     for i in alp,num,spe_ch:
         pa1=str(r.choice(alp))
         pa2=str(r.choice(num))
         pa3=str(r.choice(spe_ch))
-    i+=1
-pword=pa1+pa2+pa3
+        pword=pword+pa1+pa2+pa3
+    count=count+1
 print("Random Password is ",pword)
