78ec63de5510e1cdbab8307050660de2fc66c9cc
ideal-idiot <77572290+ideal-idiot@users.noreply.github.com>
2021-01-25 10:35:19 +0530

    Create Random_Password_Generator.py

---
+++ Random_Password_Generator.py
@@ -0,0 +1,10 @@
+import random as r
+alp=['a','b','c','d','e','f','g','h','i','j','k','l','m','o','p','q','r','s','t','u','v','w','x','y','z']
+num=[1,2,3,4,5,6,7,8,9]
+spe_ch=['!','@','#','$','%','^','&','*','(',')','+']
+for i in alp,num,spe_ch:
+    pa1=str(r.choice(alp))
+    pa2=str(r.choice(num))
+    pa3=str(r.choice(spe_ch))
+pword=pa1+pa2+pa3
+print("Random Password is ",pword)
